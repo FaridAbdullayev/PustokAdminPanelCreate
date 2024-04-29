03b1429829b8c7ab1e0efdd0b64c83706c0cd0d9
Farid Abdullayev <farid12344321>
2024-04-29 18:48:06 +0400

    homework

---
+++ PustokHomework/PustokHomework/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokHomework.Areas.Manage.ViewModels;
+using PustokHomework.Data;
+using PustokHomework.Models;
+
+namespace PustokHomework.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public IActionResult Index(int page =1)
+        {
+            var query = _context.Sliders;
+            return View(PaginatedList<Slider>.Create(query, page, 2));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Slider slider)
+        {
+            _context.Sliders.Add(slider);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
